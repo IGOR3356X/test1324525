9b540b8cbba24d299d72ed67777857664e812b26
amsmyslov <a.m.smyslov@yandex.ru>
2024-10-07 21:38:12 +0300

    change code2222

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!289347982734982374897239");
+        System.out.println("Hello world!2893479827349asjkdfbakljsbdjk82374897239");
     }
 }
